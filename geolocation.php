834983b2c248a7828896d1adab93cf4c603d7108
frsh <frsh@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-05-03 16:25:17 +0000

    git-svn-id: https://plugins.svn.wordpress.org/geolocation/tags/0.1@237127 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- geolocation.php
+++ geolocation.php
@@ -1,11 +1,11 @@
 <?php
 /*
 Plugin Name: Geolocation
-Plugin URI: http://wordpress.org/plugins/geolocation/
+Plugin URI: http://geo.chrisboyd.net/
 Description: Displays post geotag information on an embedded map.
 Version: 0.1
 Author: Chris Boyd
-Author URI: http://www.chrisboyd.net
+Author URI: http://www.chrisboyd.net/
 License: GPL2
 */
 
